1f71a55e1f7c634a7f55abd1a95f02a008d298d3
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-10 04:39:40 +0500

    some navbar styling

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
 import { routes } from "@/contents/routes";
 
 function Navbar() {
   return (
     <header className="fixed top-8 mx-auto w-full flex items-center justify-center h-16 ">
-      <div className="flex justify-center items-center bg-blue-950 gap-5 text-lg rounded-full px-5 py-2">
+      <div
+        id="navbar"
+        className={`flex justify-center items-center  bg-blue-950 gap-5 text-lg rounded-full px-5 py-2 bg-transparent border border-slate-700`}
+      >
         {routes.map((route) => {
-            return (
-                <p key={route} className="hover:bg-slate-700 rounded-full px-4 py-2">
-                {route}
-                </p>
-            );
+          return (
+            <p
+              key={route}
+              className="hover:bg-slate-700 rounded-full px-4 py-2"
+            >
+              {route}
+            </p>
+          );
         })}
-        </div>
+      </div>
     </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const navBar = document.getElementById("navbar");
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        navBar!!.classList.add("bg-slate-800");
+        navBar!!.classList.add("border");
+      } else {
+        navBar!!.classList.remove("bg-slate-800");
+        navBar!!.classList.remove("border");
+      }
+    });
+  }, []);
   return (
-    <main className="">
-        <h1 className="">Welcome to Next.js!</h1>
-    </main>
+    <section>
+      <div className="">Welcome to Next.js!</div>
+      <div className="h-screen bg-slate-900"></div>
+      <div className="h-screen bg-slate-900"></div>
+      <div className="h-screen bg-slate-900"></div>
+
+      <div className="h-screen bg-slate-900"></div>
+    </section>
   );
 }
