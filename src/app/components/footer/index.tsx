66935c87fcc2007b24b62392751d7bfb9127d009
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-14 18:36:37 +0500

    some basic styles for h1,h2,p tags

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,9 +4,9 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="w-full h-16 flex items-center justify-center px-10 xl:px-0">
-      <div className="flex flex-col-reverse gap-4 md:flex-row md:justify-between items-center w-full h-full  max-w-7xl border-t-[1px] border-foreground-secondary text-foreground-secondary text-xs">
-        <p>© {year} Zafeer Tech</p>
-        <div>
+      <div className="flex flex-col-reverse gap-4 md:flex-row md:justify-between items-center w-full h-full  max-w-7xl border-t-[1px] border-foreground-secondary text-foreground-secondary ">
+        <p className="text-xs">© {year} Zafeer Tech</p>
+        <div className="text-xs">
           <Moon className="h-5 w-5" />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,37 +14,34 @@ export default function Home() {
         }}
       >
         <div
-          className="flex flex-col gap-5 text-center items-center justify-center w-full h-full  "
+          className="flex flex-col gap-5 text-center items-center justify-center w-full h-full object-cover  "
           style={{
             backgroundImage: "url('/gradient.jpeg')",
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
             backgroundPosition: "center",
           }}
         >
-          <h1 className="text-7xl ">Hello. I'm Zafeer.</h1>
-          <h1 className="text-7xl text-primary">A Software Engineer</h1>
-          <p className="text-foreground-secondary text-lg leading-3">
+          <h1>Hello. I'm Zafeer.</h1>
+          <h1 className="text-primary">A Software Engineer</h1>
+          <p className="leading-3">
             I'm passionate about creating experiences that are easy to use,
             accessible and
           </p>
-          <p className="text-foreground-secondary text-lg leading-3">
-            that meet the users need.
-          </p>
+          <p className="leading-3">that meet the users need.</p>
         </div>
       </div>
 
       <div className="flex flex-col text-start max-w-7xl mt-28 px-10 xl:px-0">
-        <h1 className="text-2xl">About</h1>
-        <p className="text-foreground-secondary leading-6">
-          About I'm a 21 year old developer living in Germany. I discovered my
-          passion for coding after seeing my brother do it. I fell in love with
-          using my creativity to build things for the web. Through persistence,
-          self-discipline, and commitment, I achieved my goal of becoming a
-          frontend developer. What kept me on this journey is that I always find
-          learning new things exciting and facing unfamiliar challenges. In
-          addition to coding, I enjoy building side projects because it allows
-          me to explore my creativity and accomplish exciting things.
+        <h2 className="text-2xl">About</h2>
+        <p className="leading-6">
+          About I{"'"}m a 21 year old developer living in Germany. I discovered
+          my passion for coding after seeing my brother do it. I fell in love
+          with using my creativity to build things for the web. Through
+          persistence, self-discipline, and commitment, I achieved my goal of
+          becoming a frontend developer. What kept me on this journey is that I
+          always find learning new things exciting and facing unfamiliar
+          challenges. In addition to coding, I enjoy building side projects
+          because it allows me to explore my creativity and accomplish exciting
+          things.
         </p>
       </div>
     </section>
