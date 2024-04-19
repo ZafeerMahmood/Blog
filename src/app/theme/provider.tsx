0668027adf616ad99ec7cb04a81b9a2590c90cf0
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-19 22:40:55 +0500

    nav bar fix

--- src/app/theme/provider.tsx
+++ src/app/theme/provider.tsx
@@ -7,13 +7,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
       const navBar = document.getElementById("MainNav");
       if (window.scrollY > 100) {
         navBar!!.classList.add(
-          "dark:bg-[#232323]",
+          "bg-[#232323]",
           "border-[1px]",
           "border-box-border"
         );
       } else {
         navBar!!.classList.remove(
-          "dark:bg-[#232323]",
+          "bg-[#232323]",
           "border-[1px]",
           "border-box-border"
         );
