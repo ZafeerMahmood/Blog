979c3218e63772d39389fc696a8be0011567d6d5
ZafeerMahmood <zafeer7460@gmail.com>
2024-04-19 21:51:28 +0500

    fix light mode

--- src/app/components/ui/Writing.tsx
+++ src/app/components/ui/Writing.tsx
@@ -17,7 +17,7 @@ function Writing({
     <Link
       target="_blank"
       href={writings.link}
-      className="h-full w-full flex justify-items-start items-center min-h-36 bg-box border-[1px] border-box-border rounded-md gradientBG hover:border-box-accent text-start"
+      className="h-full w-full flex justify-items-start items-center min-h-36 bg-box border-[1px] border-box-border rounded-md dark:gradientBG hover:border-box-accent text-start"
     >
       <div className="flex flex-col justify-items-start text-start px-5">
         <h3 className="text-lg text-start py-1">{writings.title}</h3>
