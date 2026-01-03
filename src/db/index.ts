a8887d2b15c2645f71e1326bbcf40352d629fd28
ZafeerMahmood <zafeer7460@gmail.com>
2026-01-03 17:16:38 +0500

    add supabase db url to work with drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,5 +4,5 @@ import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL || '';
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema });
